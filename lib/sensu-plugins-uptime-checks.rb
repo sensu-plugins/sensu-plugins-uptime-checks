a295165556c73925c47178e8de59a98b1e9616c0
Matt Jones <mattjones@yieldbot.com>
2015-06-03 22:35:16 -0400

    deploy

--- lib/sensu-plugins-uptime-checks.rb
+++ lib/sensu-plugins-uptime-checks.rb
@@ -1,15 +1,1 @@
-
 require 'sensu-plugins-uptime-checks/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsUptimeChecks
-  class << self
-  end
-
-  class << self
-  end
-end
